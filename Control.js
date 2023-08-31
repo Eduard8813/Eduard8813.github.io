50760a693513a2099151f095898a3230aaf2e8f3
Eduard8813 <143306086+Eduard8813@users.noreply.github.com>
2023-08-31 09:34:11 -0600

    Update Control.js

--- Control.js
+++ Control.js
@@ -10,8 +10,8 @@ function iniciarSesion() {
 
     if (usuariosRegistrados.hasOwnProperty(usuario) && usuariosRegistrados[usuario] === contrasena) {
         alert("Inicio de sesión exitoso");
-	    redirigirSitio();
-  return false; 
+        mostrarBoton();
+        return false; 
     } else {
         alert("Credenciales incorrectas");
         return false;
@@ -21,18 +21,27 @@ function iniciarSesion() {
 function registrarUsuario() {
     var nuevoUsuario = document.getElementById("nuevo-usuario").value;
     var nuevaContrasena = document.getElementById("nueva-contrasena").value;
-	
+
     usuariosRegistrados[nuevoUsuario] = nuevaContrasena;
     guardarUsuariosRegistrados();
     alert("Usuario registrado exitosamente");
     return false; 
 }
 
- function redirigirSitio() {
-window.location.href = "":
-  }
-    var registroContainer = d1ocument.getElementById("registro-container");
-    registroContainer.classList.remove("oculto");
+function mostrarBoton() {
+    var boton = document.getElementById("boton-container");
+    boton.classList.remove("oculto");
+   
+    window.location.href = "#boton-container";
+}
 
+function redirigirSitio() {
+    window.location.href = "content://com.paprbit.dcoder.fileprovider/files/temp.html ";
+}
+
+function irARegistro() {
+    var registroContainer = document.getElementById("registro-container");
+    registroContainer.classList.remove("oculto");
+    
     window.location.href = "#registro-container";
 }
