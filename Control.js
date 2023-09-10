656218bc71ff3d03d695c3ed9071ed1590b8df5e
Eduard8813 <143306086+Eduard8813@users.noreply.github.com>
2023-09-09 21:00:57 -0600

    Update Control.js

--- Control.js
+++ Control.js
@@ -1,56 +1,121 @@
 const mainDiv = document.getElementById("main");
-const infoDiv = document.getElementById("infoDiv");
-const question = document.getElementById("question");
-const infoSi = document.getElementById("infoSi");
-const infoNo = document.getElementById("infoNo");
-const infoTalvez = document.getElementById("infoTalvez");
+    const infoDiv = document.getElementById("infoDiv");
+    const question = document.getElementById("question");
+    const infoSi = document.getElementById("infoSi");
+    const infoNo = document.getElementById("infoNo");
+    const infoTalvez = document.getElementById("infoTalvez");
 
-const btnSi = document.getElementById("btnSi");
-const btnNo = document.getElementById("btnNo");
-const btnTalvez = document.getElementById("btnTalvez");
+    const btnSi = document.getElementById("btnSi");
+    const btnNo = document.getElementById("btnNo");
+    const btnTalvez = document.getElementById("btnTalvez");
 
-const btnRetroceso = document.getElementById("btnRetroceso");
-
-function showElement(element) {
-    if (element) {
-        element.style.display = "block";
+    function showElement(element) {
+        if (element) {
+            element.style.display = "block";
+        }
     }
+
+    function hideElement(element) {
+        if (element) {
+            element.style.display = "none";
+        }
+    }
+
+    // Event listeners para los botones de plantas
+    btnSi.addEventListener("click", function() {
+        // Mostrar información específica sobre Culantro
+        hideElement(question);
+        hideElement(mainDiv);
+        showElement(infoDiv);
+        showElement(infoSi);
+        hideElement(infoNo);
+        hideElement(infoTalvez);
+    });
+
+    btnNo.addEventListener("click", function() {
+        // Mostrar información específica sobre Hierba buena
+        hideElement(question);
+        hideElement(mainDiv);
+        showElement(infoDiv);
+        hideElement(infoSi);
+        showElement(infoNo);
+        hideElement(infoTalvez);
+    });
+
+    btnTalvez.addEventListener("click", function() {
+        // Mostrar información específica sobre Sábila
+        hideElement(question);
+        showElement(mainDiv);
+        showElement(infoDiv);
+        hideElement(infoSi);
+        hideElement(infoNo);
+        showElement(infoTalvez);
+    });
+
+    // Event listeners para los botones de compra
+    const btnCompraSi = document.getElementById("btnCompraSi");
+    const btnCompraNo = document.getElementById("btnCompraNo");
+    const btnCompraTalvez = document.getElementById("btnCompraTalvez");
+	
+document.getElementById("btnActivarPlanta1").addEventListener("click", function() {
+    activarPlanta(1);
+});
+
+document.getElementById("btnActivarPlanta2").addEventListener("click", function() {
+    activarPlanta(2);
+});
+
+document.getElementById("btnActivarPlanta3").addEventListener("click", function() {
+    activarPlanta(3);
+});
+
+function activarPlanta(planta) {
+    // Realiza una solicitud HTTP POST al ESP32 para activar la planta
+    fetch('/activar_planta', {
+        method: 'POST',  // Método HTTP utilizado (POST)
+        headers: {
+            'Content-Type': 'application/json'  // Encabezados de la solicitud
+        },
+        body: JSON.stringify({ planta: planta })  // Cuerpo de la solicitud en formato JSON
+    })
+    .then(response => response.text())  // Convierte la respuesta a texto
+    .then(data => {
+        alert(data); // Muestra la respuesta del ESP32 en una ventana emergente (alert)
+        location.reload(); // Recarga la página después de activar la planta
+    })
+    .catch(error => {
+        console.error('Error:', error);  // Maneja cualquier error que ocurra durante la solicitud
+    });
 }
 
-function hideElement(element) {
-    if (element) {
-        element.style.display = "none";
-    }
-}
+    // Event listeners para los botones "Atrás"
+    const btnAtrasCulantro = document.getElementById("btnAtrasCulantro");
+    const btnAtrasHierbaBuena = document.getElementById("btnAtrasHierbaBuena");
+	const btnRetroceso = document.getElementById("btnRetroceso");
+	
 
-btnSi.addEventListener("click", function() {
-    hideElement(question);
-    hideElement(mainDiv);
-    showElement(infoDiv);
-    showElement(infoSi);
-    hideElement(infoNo);
-    hideElement(infoTalvez);
-});
+    btnAtrasCulantro.addEventListener("click", function() {
+        // Mostrar la página de selección de plantas nuevamente
+        showElement(question);
+        showElement(mainDiv);
+        hideElement(infoDiv);
+        hideElement(infoSi);
+        hideElement(infoNo);
+        hideElement(infoTalvez);
+    });
 
-btnNo.addEventListener("click", function() {
-    hideElement(question);
-    hideElement(mainDiv);
-    showElement(infoDiv);
-    hideElement(infoSi);
-    showElement(infoNo);
-    hideElement(infoTalvez);
-});
-
-btnTalvez.addEventListener("click", function() {
-    hideElement(question);
-    hideElement(mainDiv);
-    showElement(infoDiv);
-    hideElement(infoSi);
-    hideElement(infoNo);
-    showElement(infoTalvez);
-});
-
+    btnAtrasHierbaBuena.addEventListener("click", function() {
+        // Mostrar la página de selección de plantas nuevamente
+        showElement(question);
+        showElement(mainDiv);
+        hideElement(infoDiv);
+        hideElement(infoSi);
+        hideElement(infoNo);
+        hideElement(infoTalvez);
+    });
+	
 btnRetroceso.addEventListener("click", function() {
+    // Mostrar la página de selección de plantas nuevamente
     showElement(question);
     showElement(mainDiv);
     hideElement(infoDiv);
@@ -58,30 +123,3 @@ btnRetroceso.addEventListener("click", function() {
     hideElement(infoNo);
     hideElement(infoTalvez);
 });
-
-// Nuevos botones para comprar plantas
-const btnCompraUno = document.createElement("button");
-btnCompraUno.textContent = "Compra uno";
-btnCompraUno.addEventListener("click", function() {
-    alert("¡Información enviada a la ESP32 para comprar Planta uno!");
-    // Aquí puedes agregar código para enviar la solicitud al ESP32 para comprar la Planta uno
-});
-
-const btnCompraDos = document.createElement("button");
-btnCompraDos.textContent = "Compra dos";
-btnCompraDos.addEventListener("click", function() {
-    alert("¡Información enviada a la ESP32 para comprar Planta dos!");
-    // Aquí puedes agregar código para enviar la solicitud al ESP32 para comprar la Planta dos
-});
-
-const btnCompraTres = document.createElement("button");
-btnCompraTres.textContent = "Compra tres";
-btnCompraTres.addEventListener("click", function() {
-    alert("¡Información enviada a la ESP32 para comprar Planta tres!");
-    // Aquí puedes agregar código para enviar la solicitud al ESP32 para comprar la Planta tres
-});
-
-// Agregar los nuevos botones al div principal
-mainDiv.appendChild(btnCompraUno);
-mainDiv.appendChild(btnCompraDos);
-mainDiv.appendChild(btnCompraTres);
